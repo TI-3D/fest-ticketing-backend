05bd580b094ce51c322e39d7bf413553e3b365ba
Achmad Raihan Fahrezi Effendy <raihanachmad8@gmail.com>
2024-11-24 11:24:50 +0700

    fix(chore): repair docker pgadmin config and core config

--- app/core/config.py
+++ app/core/config.py
@@ -68,6 +68,8 @@ class Settings(BaseSettings):
     
     AUTH_EXCLUDED_PATHS: list[str] = [
         "/",
+        "/docs",
+        "/openapi.json",
         "/api/v1/auth/signup", 
         "/api/v1/auth/signin",  
         "/api/v1/auth/google-signin", 
